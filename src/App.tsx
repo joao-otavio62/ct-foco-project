f2756fd49e52468ba1b0feeb5ad8600c823e7557
joao <joao.otavio@hotmail.com>
2026-02-25 16:39:49 -0300

    festure: incrementando adminpage

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import { MainPage } from "./pages/MainPage";
 import { NotFoundPage } from "./pages/NotFoundPage";
+import { AdminPage } from "./pages/AdminPage";
 
 const App = () => {
   return (
     <Routes>
       <React.Fragment>
+        <Route element={<AdminPage />} path="/adminpage" />
         <Route element={<MainPage />} path="/" />
         <Route element={<NotFoundPage />} path="*" />
       </React.Fragment>
