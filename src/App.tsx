0ef34aab9d8777de28554db37a5e8ca763ad0205
joao <joao.otavio@hotmail.com>
2026-03-02 10:20:32 -0300

    feature: incrementando novas rotas e paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-import { Route, Routes } from "react-router";
-import { MainPage } from "./pages/MainPage";
-import { NotFoundPage } from "./pages/NotFoundPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
 import { AdminPage } from "./pages/AdminPage";
 
 const App = () => {
