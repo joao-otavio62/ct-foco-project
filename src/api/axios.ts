ad8096d55bd9ceb8255db7a6b9d80f1912f266da
joao <joao.otavio@hotmail.com>
2026-05-05 08:10:35 -0300

    configurando api url de producao

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5084/api", // API C#
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5084/api",
 });
 
 api.interceptors.request.use(config => {
